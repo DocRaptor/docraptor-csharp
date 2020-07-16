9004dafc54b927f662c45c58fa765c8e184fcdaf
David Ronk <david@expectedbehavior.com>
2020-07-16 15:06:03 -0400

    Updated examples
    - updated existing examples for swagger c# changes
    - created hosted document examples

--- examples/hosted_async.cs
+++ examples/hosted_async.cs
@@ -1,14 +1,23 @@
-// This example demonstrates creating a PDF using common options and saving it
-// to a place on the filesystem.
+// As a paid add-on, DocRaptor can provide long-term, publicly-accessible hosting for your documents.
+// This allows you to provide a URL to your end users, third party tools like Zapier and Salesforce,
+// or anyone else. We'll host the document on your behalf at a completely unbranded URL for as long
+// as you want, or within the limits you specify.
 //
-// It is created asynchronously, which means DocRaptor will render it for up to
-// 10 minutes. This is useful when creating many documents in parallel, or very
-// large documents with lots of assets.
+// This example demonstrates creating a PDF using common options that DocRaptor will host for you.
+// By default, hosted documents do not have limits on downloads or hosting time, though you may
+// pass additional parameters to the document generation call to set your own limits. Learn more
+// about the specific options in the hosted API documentation.
+// https://docraptor.com/documentation/api#api_hosted
+//
+// The document is created asynchronously, which means DocRaptor will allow it to generate for up to
+// 10 minutes. This is useful when creating many documents in parallel, or very large documents with
+// lots of assets.
 //
 // DocRaptor supports many options for output customization, the full list is
 // https://docraptor.com/documentation/api#api_general
 //
-// You can run this example with: ./script/run_csharp_file examples/async.cs
+// You can run this example with: ./script/run_csharp_file examples/hosted_async.cs
+
 
 using DocRaptor.Client;
 using DocRaptor.Model;
@@ -20,7 +29,7 @@ using System.Threading;
 class AsyncTest {
   static void Main(string[] args) {
     try {
-      Configuration.Default.Username = "YOUR_API_KEY_HERE"; // this key works for test documents
+      Configuration.Default.Username = "YOUR_API_KEY_HERE"; // you will need a real api key to test hosted documents
       DocApi docraptor = new DocApi();
 
       Doc doc = new Doc(
@@ -36,7 +45,7 @@ class AsyncTest {
         // )
       );
 
-      AsyncDoc response = docraptor.CreateAsyncDoc(doc);
+      AsyncDoc response = docraptor.CreateHostedAsyncDoc(doc);
 
       DocStatus status_response;
       Boolean done = false;
--- examples/hosted_sync.cs
+++ examples/hosted_sync.cs
@@ -1,15 +1,22 @@
-// This example demonstrates creating a PDF using common options and saving it
-// to a place on the filesystem.
+// As a paid add-on, DocRaptor can provide long-term, publicly-accessible hosting for your documents.
+// This allows you to provide a URL to your end users, third party tools like Zapier and Salesforce,
+// or anyone else. We'll host the document on your behalf at a completely unbranded URL for as long
+// as you want, or within the limits you specify.
 //
-// It is created synchronously, which means DocRaptor will render it for up to
-// 60 seconds. It is slightly simpler than making documents using the async
-// interface but making many documents in parallel or very large documents with
-// lots of assets will require the async api.
+// This example demonstrates creating a PDF that DocRaptor will host for you using common options.
+// By default, hosted documents do not have limits on downloads or hosting time, though you may
+// pass additional parameters to the document generation call to set your own limits. Learn more
+// about the specific options in the hosted API documentation.
+// https://docraptor.com/documentation/api#api_hosted
+//
+// It is created synchronously, which means DocRaptor will allow it to generate for up to 60 seconds.
+// Since this document will be hosted by DocRaptor the response from this request will return a JSON
+// formatted object containing public URL where the document can be downloaded from.
 //
 // DocRaptor supports many options for output customization, the full list is
 // https://docraptor.com/documentation/api#api_general
 //
-// You can run this example with: ./script/run_csharp_file examples/sync.cs
+// You can run this example with: ./script/run_csharp_file examples/hosted_sync.cs
 
 using DocRaptor.Client;
 using DocRaptor.Model;
@@ -21,7 +28,7 @@ using System.Threading;
 class SyncTest {
   static void Main(string[] args) {
     try {
-      Configuration.Default.Username = "YOUR_API_KEY_HERE"; // this key works for test documents
+      Configuration.Default.Username = "YOUR_API_KEY_HERE"; // you will need a real api key to test hosted documents
       DocApi docraptor = new DocApi();
 
       Doc doc = new Doc(
@@ -37,8 +44,9 @@ class SyncTest {
         // )
       );
 
-      byte[] create_response = docraptor.CreateDoc(doc);
-      File.WriteAllBytes("/tmp/docraptor-csharp.pdf", create_response);
+      DocStatus status_response = docraptor.CreateHostedDoc(doc);
+      byte[] data = docraptor.GetAsyncDoc(status_response.DownloadId);
+      File.WriteAllBytes("/tmp/docraptor-csharp.pdf", data);
       Console.WriteLine("Wrote PDF to /tmp/docraptor-csharp.pdf");
     } catch (DocRaptor.Client.ApiException error) {
       Console.WriteLine(error);
