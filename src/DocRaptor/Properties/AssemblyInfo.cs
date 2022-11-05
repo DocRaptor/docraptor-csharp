62fe36a4132c81874f8e54623e4e959035d1868b
Jason Gladish <jason@expectedbehavior.com>
2022-11-04 20:43:27 -0400

    Actually generate code with openapi
    Why is this change needed?
    --------------------------
    Swagger isn't being maintained. We needed to switch to openapi for
    other agents, and if it works it's nice for all the agents to use the
    same tech.
    
    How does it address the issue?
    ------------------------------
    This is just the result of `script/generate_language`.
    
    Any links to any relevant tickets, articles, or other resources?
    ---------------------------------------------------------------
    https://3.basecamp.com/3093825/buckets/29124557/todos/5284412189
    https://3.basecamp.com/3093825/buckets/29124557/todos/5284421778
    
    Any screenshots?
    ----------------
    
    Did you complete all of the following?
    --------------------------------------
    - Run test suite?
    - Add new tests?
    - Consider security implications and practices?

--- src/DocRaptor/Properties/AssemblyInfo.cs
+++ src/DocRaptor/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Swagger Library")]
-[assembly: AssemblyDescription("A library generated from a Swagger doc")]
+[assembly: AssemblyTitle("OpenAPI Library")]
+[assembly: AssemblyDescription("A library generated from a OpenAPI doc")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Swagger")]
-[assembly: AssemblyProduct("SwaggerLibrary")]
+[assembly: AssemblyCompany("OpenAPI")]
+[assembly: AssemblyProduct("OpenAPILibrary")]
 [assembly: AssemblyCopyright("No Copyright")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
