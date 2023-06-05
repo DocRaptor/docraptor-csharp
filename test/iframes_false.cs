6711c32467fc915b2ce719d45998054baa37ff46
Jason Gladish <jason@expectedbehavior.com>
2023-06-04 22:26:57 -0400

    Add tests for the new iframes option
    Why is this change needed?
    --------------------------
    We want to have a basic check that this new works and continues to work
    in the future.
    
    How does it address the issue?
    ------------------------------
    Add iframes tests.
    
    Any links to any relevant tickets, articles, or other resources?
    ---------------------------------------------------------------
    https://3.basecamp.com/3093825/buckets/29007795/todolists/5257298600
    
    Did you complete all of the following?
    --------------------------------------
    - Run test suite?
    - Add new tests?
    - Consider security implications and practices?

---
+++ test/iframes_false.cs
@@ -0,0 +1,57 @@
+using DocRaptor.Client;
+using DocRaptor.Model;
+using DocRaptor.Api;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Diagnostics;
+
+class SyncTest {
+  static void Main(string[] args) {
+    DocApi docraptor = new DocApi();
+    docraptor.Configuration.Username = "YOUR_API_KEY_HERE";
+    // docraptor.Configuration.Debug = true; // Not supported in Csharp
+
+    Doc doc = new Doc(
+      name: "csharp-iframes.pdf",
+      test: true,
+      documentContent: "" +
+        "<html><body>" +
+          "<p>Baseline text</p>" +
+          "<iframe src=\"https://docraptor-test-harness.herokuapp.com/public/index.html\"></iframe>" +
+        "</body></html>",
+      documentType: Doc.DocumentTypeEnum.Pdf,
+      princeOptions: new PrinceOptions(
+        iframes: false
+      )
+    );
+
+    byte[] document = docraptor.CreateDoc(doc);
+    string output_file = Environment.GetEnvironmentVariable("TEST_OUTPUT_DIR") +
+      "/" + Environment.GetEnvironmentVariable("TEST_NAME") + "_csharp_" +
+      Environment.GetEnvironmentVariable("RUNTIME_ENV") + ".pdf";
+    File.WriteAllBytes(output_file, document);
+
+    string command = "pdftotext " + output_file + " -";
+    var proc = new Process
+    {
+        StartInfo = new ProcessStartInfo
+        {
+            FileName = "/bin/bash",
+            Arguments = "-c \""+ command + "\"",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true
+        }
+    };
+    proc.Start();
+    string output = proc.StandardOutput.ReadToEnd();
+    proc.WaitForExit();
+
+    if(output.Contains("Test")) {
+      Console.WriteLine("output shouldn't have contained iframe content: " + output);
+      Environment.Exit(1);
+    }
+  }
+}
